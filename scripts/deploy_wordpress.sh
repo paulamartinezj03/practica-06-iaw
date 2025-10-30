8587b9593bf2e740fb17f3b4396e7edc0b899227
paula <paula@gmail.com>
2025-10-30 11:30:15 +0000

    cambios

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -2,3 +2,24 @@
 set -ex
 #Importamos archivo .env
 source .env
+#Eliminamos descargas previas
+rm -f /tmp/latest.*
+#Descargamos el código fuente de wordpress
+wget https://wordpress.org/latest.zip -P /tmp
+#Descomprimir zip
+apt update
+apt install unzip -y
+rm -rf /tmp/wordpress
+unzip /tmp/latest.zip -d /tmp
+#Movemos los archivos que hemos descomprimido a /var/www/html
+rm -rf /var/www/html/*
+mv /tmp/wordpress/* /var/www/html/
+#Creamos una base de datos de ejemplo
+mysql -u root -e "DROP DATABASE IF EXISTS $DB_NAME"
+mysql -u root -e "CREATE DATABASE $DB_NAME";
+
+#Creamos un usuario y contraseña para la base de datos
+mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'%';";
+mysql -u root -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD'";
+#Le asignamos privilegios de nuestra base de datos
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'";
