ad9c4603f4ad31799a95e5d886831ad7a4b87c4f
paula <paula@gmail.com>
2025-11-04 08:59:58 +0000

    cambios finales

--- scripts/deploy_wordpress.sh
+++ scripts/deploy_wordpress.sh
@@ -19,7 +19,15 @@ mysql -u root -e "DROP DATABASE IF EXISTS $DB_NAME"
 mysql -u root -e "CREATE DATABASE $DB_NAME";
 
 #Creamos un usuario y contraseña para la base de datos
-mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'%';";
-mysql -u root -e "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD'";
+mysql -u root -e "DROP USER IF EXISTS '$DB_USER'@'$IP_CLIENTE_MYSQL';";
+mysql -u root -e "CREATE USER $DB_USER@'$IP_CLIENTE_MYSQL' IDENTIFIED BY '$DB_PASSWORD'";
 #Le asignamos privilegios de nuestra base de datos
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$IP_CLIENTE_MYSQL'";
+#Creamos el archivo deconfiguracion de wordpress
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+#Configuramos las variables del archivo php
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/wp-config.php
+#Modificamos los permisos del directorio /var/www/html
+chown -R www-data:www-data /var/www/html
